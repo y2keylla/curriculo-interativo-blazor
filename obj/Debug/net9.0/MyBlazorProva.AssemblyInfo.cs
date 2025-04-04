a60b4446d24367ab9d0405f1b6ab68c784123720
@y2keylla <keylla.bispo@sou.inteli.edu.br>
2025-04-04 10:21:50 -0300

    feat: adiciona ci-cd

--- obj/Debug/net9.0/MyBlazorProva.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyBlazorProva.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorProva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b4bed388d7806ba9e3a96d9b49c660dd62f5078")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorProva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorProva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
